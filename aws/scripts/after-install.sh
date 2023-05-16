b630d2925b75368de7e34d667876b37cc215ab95
Gajendra1660 <132563495+Gajendra1660@users.noreply.github.com>
2023-05-16 12:00:19 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,5 +4,4 @@ set -xe
 
 # Copy war file from S3 bucket to tomcat webapp folder
 aws s3 cp s3://codepipeline-ap-south-1-261437824404/ttttttpipe/SourceArti/
-# Ensure the ownership permissions are correct.
-chown -R tomcat:tomcat /usr/local/tomcat9/webapps
+# Ensure the ownership permissions are correct
